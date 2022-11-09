c8d018ebbf9c22bbcbda611e71da485bbe141342
OnyxX <113524230+DONNAES@users.noreply.github.com>
2022-11-09 15:22:05 +0100

    Add files via upload

--- Web/assets/SQL/User.sql
+++ Web/assets/SQL/User.sql
@@ -2,10 +2,10 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Nov 09, 2022 at 11:06 AM
--- Server version: 5.7.34
--- PHP Version: 7.4.21
+-- Hôte : localhost:8889
+-- Généré le : mer. 09 nov. 2022 à 14:21
+-- Version du serveur :  5.7.34
+-- Version de PHP : 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,43 +18,53 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `DB_TPOM`
+-- Base de données : `DB_TPOM`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `User`
+-- Structure de la table `User`
 --
 
 CREATE TABLE `User` (
   `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `password` text NOT NULL,
   `nickname` varchar(255) NOT NULL,
   `user_creation` datetime NOT NULL,
-  `last_connection` datetime NOT NULL
+  `last_connection` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Déchargement des données de la table `User`
+--
+
+INSERT INTO `User` (`id`, `email`, `password`, `nickname`, `user_creation`, `last_connection`) VALUES
+(1, 'pipo@gg.com', 'azerty', 'pipette', '1989-05-08 06:06:06', NULL),
+(2, 'papillote@gg.com', 'qwerty', 'pipelette', '2002-08-11 06:06:08', NULL);
+
+--
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `User`
+-- Index pour la table `User`
 --
 ALTER TABLE `User`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `nickname` (`nickname`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `User`
+-- AUTO_INCREMENT pour la table `User`
 --
 ALTER TABLE `User`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
