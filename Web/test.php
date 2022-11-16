fe18e8840a25de0862ff1a0d732eec36dc6ca2a8
Naaaokii <eloitranchant@mbp-de-admin-2.edu.sir>
2022-11-16 10:22:29 +0100

    testtt

--- Web/test.php
+++ Web/test.php
@@ -1,5 +1,5 @@
 <?php
-require('./assets/includes/database.inc.php')
+require('assets/includes/database.inc.php')
 
 if(isset($_Test['username'] || isset($_Test['email'])))
 {
@@ -11,30 +11,48 @@ if(isset($_Test['username'] || isset($_Test['email'])))
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="register.css">
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>The Power Of Memory - Register</title>
     </head>
     <body>
+        <header>
+            <?php
+                include 'assets/view/header.inc.php'
+            ?>
+        </header>
+        <section class="backgroundImage">
+            <h1>INSCRIPTION</h1>
+        </section>
         <div class="register">
             <form>
                 <div>
-                    <input type="email" id="email" placeholder="Email" required>
+                    <input type="email" id="Email" placeholder="Email" required>
                 </div>
                 <div>
-                    <input type="text" id="pseudo" placeholder="Pseudo" required>
+                    <input type="text" id="Pseudo" placeholder="Pseudo" required>
                 </div>
                 <div>
-                    <input type="password" id="password" placeholder="Mot de passe" required>
+                    <input type="password" id="Password" placeholder="Mot de passe" required>
                 </div>
                 <div>
                     <input type="password" id="Confirmpassword" placeholder="Confirmer le mot de passe" required>
                 </div>
                 <div class="button">
-                    <button class="bouton" type="submit">Inscription</button><a href="login.html" class="connexion">Connexion</a>
+                    <button class="bouton" type="submit">Inscription</button><a href="login.php" class="connexion">Connexion</a>
                 </div>
             </form>
         </div>
+
+        <a class="gotopbtn" href="#"><i class="fa-solid fa-angle-up"></i></a>
+        
+        <footer>
+            <?php
+                include 'assets/view/footer.inc.php'
+            ?>
+        </footer>
     </body>
 </html>
