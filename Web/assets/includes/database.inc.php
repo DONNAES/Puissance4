31ff63bdeb05212405cd9498034a6266bb8d8eb3
Naaaokii <eloitranchant@mbp-de-admin-2.edu.sir>
2022-11-16 09:52:56 +0100

    test

--- Web/assets/includes/database.inc.php
+++ Web/assets/includes/database.inc.php
@@ -1,3 +1,6 @@
-<?php 
-$dbh = new PDO('mysql:host=localhost;dbname=DB_TPOM', $user, $pass); 
+<?php
+
+$user = "root";
+$pass = "root";
+$dbh = new PDO('mysql:host=localhost;dbname=The_Power_Of_Memory', $user, $pass);
 ?>
