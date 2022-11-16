1cd86aa2a7222a530902c764dcf167aaefa7e46e
KyoShred <lucas.salaun2@gmail.com>
2022-11-16 12:25:35 +0100

    test

--- Web/assets/includes/database.inc.php
+++ Web/assets/includes/database.inc.php
@@ -1,9 +1,6 @@
 <?php
-$username = "root";
-$password = "";
+$user = "root";
+$pass = "root";
 $errors = array();
-$dbh = new PDO('mysql:host=localhost;dbname=DB_TPOM', $username, $password);
-if($dbh === false){
-    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
-}
+$dbh = new PDO('mysql:host=localhost;dbname=DB_TPOM', $user, $pass);
 ?>
