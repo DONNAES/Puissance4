8fce38ea3b1dcf6bc7dc249a1ee02b668d9f9e68
Naaaokii <eloitranchant@macbook-pro-de-admin-2.home>
2022-11-20 15:22:58 +0100

    suppression .inc.php

--- Web/login.php
+++ Web/login.php
@@ -1,5 +1,31 @@
 <?php
-    include 'assets/includes/login.inc.php'
+    require ('./assets/includes/database.inc.php');
+
+    if (isset($_POST['email']) AND isset($_POST['password']))
+    {
+        $mail     = $_POST['email'];
+        $password = $_POST['password'];
+
+        if (!empty($mail) AND !empty($password))
+        {
+
+            
+            $hashpassword = hash('sha256', $password);
+            $req = $dbh->prepare('SELECT * FROM user WHERE email = :toto and password = :titi');
+            $req-> execute(array('toto' => $mail, 'titi' => $hashpassword));
+
+            $resultat = $req->fetch(); 
+        }
+        if (!$resultat){
+            echo 'Email ou mot de passe invalide';
+        }
+        else
+    {
+        $user_id = ('SELECT id FROM user WHERE email= $mail');
+        header('Location: index.php');
+        echo 'Vous êtes connecté';
+    }
+    }
 ?>
 
 <!DOCTYPE html>
