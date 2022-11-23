ddc5df9695830bf6fbdee608c42b9d5abfcb8a62
Scorpio <leovejux95@gmail.com>
2022-11-23 11:21:46 +0100

    modifs

--- Web/assets/js/memory.js
+++ Web/assets/js/memory.js
@@ -1,11 +1,11 @@
 const divResultat = document.querySelector("#resultat");
 
 var tabJeu= [
-    [0,0,0,0,0],
-    [0,0,0,0,0],
+    [0,1,0,0,0],
+    [0,2,0,0,0],
     [0,0,0,0,0],
     [0,0,0,0,0]
-]
+];
 
 afficherTableau();
 
@@ -13,15 +13,15 @@ function afficherTableau() {
     var txt = "";
 
     for (var i=0; i < tabJeu.length ; i++) {
-
+        txt += "<div>";
         for (var j=0; j < tabJeu[i].length ; j++) {
             if (tabJeu[i][j] === 0) {
                 txt += "<button class='btn btn-primary m-2' style='width:100px;height:100px'>Afficher</button>";
             } else {
-                txt += "img src =''"
+                txt += "<img src ='../../assets/images/memory_images/jeux_videos/ghost.png' style='width:100px;height:100px' class='m-2'>";
             }
         }
-        txt += "<div>";
+        txt += "</div>";
     }
 
     divResultat.innerHTML = txt;
