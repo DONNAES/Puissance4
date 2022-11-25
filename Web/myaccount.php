ded8b272f7ffaafc1ca682904981d4f19dbe356c
DONNAES <timeo.donnaes@gmail.com>
2022-11-25 08:54:56 +0100

    mail

--- Web/myaccount.php
+++ Web/myaccount.php
@@ -15,32 +15,36 @@
     </head>
     <body>
         <?php
-            if (isset($_POST['email']) AND isset($_POST['newmail']) AND isset($_POST['password']) AND isset($_POST['confirmpassword']))
+            if (isset($_POST))
             {
-                $mail     = $_POST['email'];
-                $newmail  = $_POST['newmail'];
-                $password = $_POST['password'];
-                $confirmpassword = $_POST['confirmpassword'];
+                if (isset($_POST['email']) AND isset($_POST['newmail']) AND isset($_POST['password']) AND isset($_POST['confirmpassword']))
+                {
+                    $mail     = $_POST['email'];
+                    $newmail  = $_POST['newmail'];
+                    $password = $_POST['password'];
+                    $confirmpassword = $_POST['confirmpassword'];
+                    
+                    $hihi = 'SELECT * FROM user WHERE email= :toto and password = :pass';
+                    $sth = $dbh->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+                    $sth->execute(['toto' => $newmail, 'pass' => $password]);
+                    $red = $sth->fetch();
+
+
                 
-                $hihi = 'SELECT * FROM user WHERE email= :toto and password = :pass';
-                $sth = $dbh->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-                $sth->execute(['toto' => $newmail, 'pass' => $password]);
-                $red = $sth->fetch();
+                    if($password != $confirmpassword){
+                        echo "Les mots de passe doivent être identiques";
+                        return false;
+                    }
+                    
+                    elseif($hihi){
+                        echo "adresse mail inexistante";
+                        return false;
+                    }
 
+                    else{
+                        $sql = 'UPDATE user SET email = "'.$newmail.'"';
+                    }
 
-            
-                if($password != $confirmpassword){
-                    echo "Les mots de passe doivent être identiques";
-                    return false;
-                }
-                
-                elseif($hihi){
-                    echo "adresse mail inexistante";
-                    return false;
-                }
-
-                else{
-                    $sql = 'UPDATE user SET email = "'.$newmail.'"';
                 }
 
             }
@@ -102,7 +106,7 @@
                 <input class="button"
                     type="submit"
                     name="reg_user"
-                    placeholder="Inscription">
+                    placeholder="change_mail">
                 </div>
             </section>
             <section class="passform"> <!--Password-->
